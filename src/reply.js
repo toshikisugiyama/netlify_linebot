3f77e28d54e5594f444a1feda116775f4f574e59
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-03-12 16:26:52 +0900

    add src/reply.js

---
+++ src/reply.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+exports.handler = async function (event, context, callback) {
+  const webhookBody = JSON.parse(event.body)
+  console.log(webhookBody)
+
+  const data = {
+    replyToken: webhookBody.events[0].replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: 'Hello Netlify Bot'
+      }
+    ]
+  }
+  console.log(data)
+
+  const res = await axios.post('https://api.line.me/v2/bot/message/reply', data, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
+    }
+  })
+  console.log(res)
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(event)
+  })
+}
