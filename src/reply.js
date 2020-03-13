d35a9ee34a8849ba18afe3ca4877d093058980f4
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-03-13 09:55:32 +0900

    try to solve an error

--- src/reply.js
+++ src/reply.js
@@ -1,30 +1,39 @@
 import axios from 'axios'
 
 exports.handler = async function (event, context, callback) {
-  const webhookBody = JSON.parse(event.body)
-  console.log(webhookBody)
+  try{
+    const webhookBody = JSON.parse(event.body)
+    console.log(webhookBody)
 
-  const data = {
-    replyToken: webhookBody.events[0].replyToken,
-    messages: [
-      {
-        type: 'text',
-        text: 'Hello Netlify Bot'
-      }
-    ]
-  }
-  console.log(data)
-
-  const res = await axios.post('https://api.line.me/v2/bot/message/reply', data, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
+    const data = {
+      replyToken: webhookBody.events[0].replyToken,
+      messages: [
+        {
+          type: 'text',
+          text: 'Hello Netlify Bot'
+        }
+      ]
     }
-  })
-  console.log(res)
+    console.log(data)
 
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(event)
-  })
+    const res = await axios.post('https://api.line.me/v2/bot/message/reply', data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
+      }
+    })
+    console.log(res)
+
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(event)
+    })
+
+  } catch (error) {
+    const {
+      status,
+      statusText
+    } = error.response
+    console.log(`Error! Http status: ${status} ${statusText}`)
+  }
 }
